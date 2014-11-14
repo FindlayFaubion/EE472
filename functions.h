5191b2d16410d35349c3ad37926d435ad1c047cd
FindlayFaubion <faubifin@uw.edu>
2014-11-13 17:20:22 -0800

    Lab 3 Complete
    All working. Some stuff messy (serial com)

--- functions.h
+++ functions.h
@@ -1,79 +1,170 @@
-#ifndef HEADER_F 
-#define HEADER_F
 
+#include "stdbool.h"
 #include "inc/hw_types.h"
+#include "inc/hw_ints.h"
 #include "driverlib/debug.h"
 #include "driverlib/sysctl.h"
+#include "driverlib/systick.h"
+#include "inc/hw_nvic.h"
 #include "drivers/rit128x96x4.h"
-
-//Include hardware memory map, GPIO driver, and PWM driver
 #include "inc/hw_memmap.h"
+#include "driverlib/pin_map.h"
 #include "driverlib/gpio.h"
 #include "driverlib/pwm.h"
-
-//Define boolean type 
-enum myBool { FALSE = 0, TRUE = 1 };
-typedef enum myBool bool;
+#include "driverlib/interrupt.h"
+#include "driverlib/uart.h"
+#include "driverlib/timer.h"
 
 // random seed
 extern int seed;
 
 // Declare global state variables
-extern bool north;
-extern bool east;
-extern bool west;
+extern bool serial_flag;
 extern bool gridlock;
 extern bool trainPresent;
 extern unsigned int trainSize;
 extern unsigned int globalCount;
+extern bool gridlockChecked;
+extern int dir_to;
+extern int dir_from;
+extern int pass_count;
+extern int pulse_count;
 
-//Null point
-#define NULL 0
+// Define common constants
+#define OLED_FREQ 1000000
+#define ASCII_OFFSET 48 // offset ascii to display numbers
+#define TRAIN_SIZE_MIN 2  // min train size
+#define TRAIN_SIZE_MAX 9  // max train size
+#define GLOBAL_CNT_PER_MIN 120 // global counts per minute
+#define CLEAR_SCREEN "                        \0"
+#define NA "N/A    \0"
+#define FREQUENCY 75 //frequency used by train buzzer
+#define OLED_LEVEL 15 //brightness level for the OLED 
 
-// offset ascii to display numbers
-#define ASCII_OFFSET 48
+// Hardware constants
+#define BUTTON_PINS 0x000000FF
+#define PULSE_PIN 0x00000004
 
-//NorthTraindata
+// global count lengths for the train light and sound
+#define NTLIGHT_LEN 6 
+#define NTSOUND_LEN 20
+#define ETLIGHT_LEN 8
+#define ETSOUND_LEN 26
+#define STLIGHT_LEN 4
+#define STSOUND_LEN 24
+#define WTLIGHT_LEN 4
+#define WTSOUND_LEN 14
+
+// Display coordinates 
+#define BASE_X 0 
+#define BASE_Y 0
+#define SHIFT_Y 10
+#define SHIFT_X 65
+#define PRES 0*SHIFT_Y 
+#define SZ 1*SHIFT_Y
+#define FROM 2*SHIFT_Y 
+#define TO 3*SHIFT_Y
+#define PASS 4*SHIFT_Y
+#define GLOB 5*SHIFT_Y
+#define GRIDL 6*SHIFT_Y
+#define UART_SHIFT 15
+#define UART_STR_LEN 27
+
+////NorthTraindata
+//typedef struct {
+//    unsigned char* light; //assuming globalcount++ = 0.5 s
+//    unsigned char* sound;
+//    int lightlen;
+//    int soundlen;
+//    int i; //count for delays and sound
+//} northTrainData;
+//
+////EastTraindata
+//typedef struct {
+//    unsigned char* light; 
+//    unsigned char* sound;
+//    int i;
+//} eastTrainData;
+//
+////SouthTraindata
+//typedef struct {
+//    unsigned char* light; 
+//    unsigned char* sound;
+//    int i;
+//} southTrainData;
+//
+////WestTraindata
+//typedef struct {
+//    unsigned char* light; 
+//    unsigned char* sound;
+//    int i;
+//} westTrainData;
+
+//CurrentTrainData
 typedef struct {
-    int* light; //assuming globalcount++ = 0.5 s
-    int* sound;
-    int i;//counter variable to count through sound; can I initialize it here?
-} northTrainData;
+    unsigned char* light; 
+    unsigned char* sound;
+    int lightlen;
+    int soundlen;
+    int i;
+} currentTrainData;
 
-//EastTraindata
+//switchControlData
 typedef struct {
-    int* light; //assuming globalcount++ = 0.5 s
-    int* sound;
-    int i;//counter variable to count through sound
-} eastTrainData;
+    unsigned char* light; 
+    unsigned int i;
+    unsigned int delay;
+    bool gridlockChecked;
+} switchControlData;
 
-//WestTraindata
-typedef struct {
-    int* light; //assuming globalcount++ = 0.5 s
-    int* sound;
-    int i;//counter variable to count through sound
-} westTrainData;
+// schedule data
+//typedef struct {
+//    int clock_f;
+//} scheduleData;
 
 //TCB
 typedef struct {
-  void* d;
-  void (*f)(void* data);
+  void* y;
+  void (*x)(void*);
 } tcb;
 
 // Declare global task data
-extern northTrainData ntd;
-extern eastTrainData etd;
-extern westTrainData wtd;
+extern currentTrainData ntd;
+extern currentTrainData etd;
+extern currentTrainData std;
+extern currentTrainData wtd;
+extern switchControlData scd;
+//extern scheduleData sd;
 
-//Function prototypes
+// global train data
+extern tcb taskArray[4];
+extern currentTrainData trains[4];
+
+// Train control 
+void Schedule();
 void TrainCom (void* data);
 void SwitchControl (void* data);
-void NorthTrain (void* data);
-void WestTrain (void* data);
-void EastTrain (void* data);
-void Schedule (void* data);
+void CurrentTrain (void* data);
+void SerialCom (void* data); 
 
-// helper functions
+// System related
+void Startup();
+void InitBuzzer(int freq);
+
+// Interrupt handlers
+void ButtonHandler();
+void IntTimer1();
+void PulseCount();
+
+// Misc helper functions
 int RandomInt(int low, int high);
-
-#endif
+void IntToString(int num, char* string, int len);
+void GetDirection(int dir, char word[]);
+//void GetDirectionArray(int dir, char word[]);
+int GetPassengers(int freq);
+void SetNTData(unsigned char*, unsigned char*);
+void SetETData(unsigned char*, unsigned char*);
+void SetSTData(unsigned char*, unsigned char*);
+void SetWTData(unsigned char*, unsigned char*);
+void SetSCData(unsigned char*);
+void UARTSend(const unsigned char*, unsigned long);
